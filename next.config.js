d18fba08d4cc1a8e34997322c4e329ffd84af18a
choriyevislombek8-png <choriyevislombek8@gmail.com>
2026-02-12 00:58:51 +0500

    Commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-{
-  "name": "animanspace",
-  "version": "1.0.0",
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start"
-  },
-  "dependencies": {
-    "next": "^14.0.0",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0"
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
